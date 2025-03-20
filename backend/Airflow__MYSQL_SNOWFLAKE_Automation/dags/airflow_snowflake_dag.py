3df17757135482d91fdaaf590ab57023a67e4a8c
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2025-03-20 21:48:27 +0530

    loading data to snowflake(last step of etl pipeline)

--- backend/Airflow__MYSQL_SNOWFLAKE_Automation/dags/airflow_snowflake_dag.py
+++ backend/Airflow__MYSQL_SNOWFLAKE_Automation/dags/airflow_snowflake_dag.py
@@ -6,15 +6,20 @@ from airflow.utils.dates import days_ago
 import pandas as pd
 from datetime import datetime
 import logging
+import pandas as pd
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from snowflake.connector.pandas_tools import write_pandas
 
 
 MYSQL_CONN_ID="mysql_default"
+SNOWFLAKE_CONN_ID="snowflake_default"
+WAREHOUSE_NAME = "FINANCEWAREHOUSE" 
 
 # DAG definition
 with DAG(
     dag_id="mysql_to_snowflake_etl",
     start_date=days_ago(1),
-    schedule_interval="@daily",
+    schedule_interval="25 15 * * *",  # ⏰ 9:00 PM daily in ist but uts-> 15:25 
     catchup=False,
     tags=["etl", "mysql", "snowflake"]
 ) as dag:
@@ -45,12 +50,32 @@ with DAG(
     @task()
     def load_data(transformed_data):
         df = pd.read_json(transformed_data, orient="split")
-        hook = SnowflakeHook(snowflake_conn_id="snowflake_default")
-        conn = hook.get_sqlalchemy_engine()
+        hook = SnowflakeHook(snowflake_conn_id=SNOWFLAKE_CONN_ID)
+        conn = hook.get_conn()
+        cursor = conn.cursor()
 
-        # Write to Snowflake (append mode)
-        df.to_sql('target_table', con=conn, index=False, if_exists='append')
+        try:
+            # Set context: warehouse, database, schema
+            cursor.execute(f"USE WAREHOUSE FINANCEWAREHOUSE")
+            cursor.execute("USE DATABASE FINANCE_DATABASE_SNOWFLAKE")
+            cursor.execute("USE SCHEMA TRANSACTIONS")
 
+            success, nchunks, nrows, _ = write_pandas(
+                conn=conn,
+                df=df,
+                table_name="transaction_statement",
+                auto_create_table=True
+            )
+
+            if success:
+                logging.info(f"✅ Successfully loaded {nrows} rows to Snowflake.")
+            else:
+                logging.error("❌ Failed to load data into Snowflake.")
+                raise Exception("Failed to load data into Snowflake")
+
+        finally:
+            cursor.close()
+            conn.close()
     # DAG flow
     raw = extract_data()
     transformed = transform_data(raw)
